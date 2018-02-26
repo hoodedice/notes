1443e176bf67e2b69d8f3c8d582012c622218d24
hoodedice <hoodedice@gmail.com>
2018-02-26 16:03:01 -0500

    remove unnecessary commented lines

--- app.js
+++ app.js
@@ -13,10 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//console.log(views);
 app.use(partials());
 app.set('view engine', 'ejs');
-//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
