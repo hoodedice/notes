ad3216b911d6189267ff30f57d1f408f80e8f30d
hoodedice <hoodedice@gmail.com>
2018-06-11 12:27:35 +0400

    add /username/noteid route since /options cannot be optional in a route pass the datetime of the note to the rendering engine

--- routes/viewNote.js
+++ routes/viewNote.js
@@ -11,7 +11,10 @@ async function getLatestNote(note_id, username) {
 
   try {
     const [result, fields] = await db.connect(GETNOTEquery, [note_id, username]);
-    return result[0][0];
+    if (result[0].length > 1) throw new Error("Username Doesn't Exist");
+    if (result[0][0] == null) return await null;
+    const note = { ...result[0][0] };
+    return await note;
   } catch (err) { throw err; }
 
 }
@@ -20,28 +23,56 @@ async function getLatestNote(note_id, username) {
 router.get('/', function (req, res, next) {
   res.redirect('/index');
 });
- 
-router.get('/:username/:noteid/', async function (req, res, next) {
+
+router.get('/:username/:noteid', async function (req, res, next) {
   try {
-    const result = await getLatestNote(req.params.noteid, req.params.username);
-    //console.log(result);
-    if (result.length == 0 || result == null) {
+    const note = await getLatestNote(req.params.noteid, req.params.username);
+    if (note == null) {
+      res.render('error', {
+        status: 404,
+        message: "Note not found at this URL",
+        error: null, stack: null
+      });
+      //res.status(404).send("Note not found");
+    }
+    else {
+      let datetime = 
+      res.render('view_note.ejs', {
+        title: note.title,
+        content: note.content,
+        description: note.description,
+        t_uploaded: note.datetime
+      });
+    }
+  } catch (err) { throw err; }
+});
+
+router.get('/:username/:noteid/:options', async function (req, res, next) {
+  try {
+    const note = await getLatestNote(req.params.noteid, req.params.username);
+    console.log(note);
+    if (note.length == 0 || note == null) {
       res.status(404).send("Note not found");
     }
     else {
       //TODO: use Notes object
-      res.render('view_note.ejs', {
-        title: result.title,
-        content: result.content,
-        description: result.description
-      });
-    }
+      switch (req.params.options) {
+
+        case "raw":
+          res.render('raw_note.ejs', {
+            content: note.content
+          });
+          break;
+
+        default:
+          throw new Error("viewNote switch Error");
+      }//end switch
+    }//end else 
   } catch (err) {
+    console.log(err);
     res.status(404).send("Note not found");
   }
 
 });
-
-//TODO: raw note text "/raw/noteid/"
 
 module.exports = router;
