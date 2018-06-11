2e182038ae0482302a0f4c95a3df4a04cffb5028
hoodedice <hoodedice@gmail.com>
2018-06-11 12:24:15 +0400

    Actually check if passwords match on server-side

--- routes/register.js
+++ routes/register.js
@@ -1,33 +1,35 @@
 var express = require('express');
 var router = express.Router();
+
 var db = require('./Database');
 var psw = require('./Password');
 
 var User = require('./User').User;
 
+
 router.get('/', function (req, res) {
-  res.render('register.ejs');
+  res.render('register.ejs', { message : null });
 });
 
-
+/** checks if the user already exists on the database **/
 async function CheckForPreExistingUsers(username, email) {
-  const DUPLICATEquery = 'SELECT `username`, `email` FROM `users` WHERE `username` = ? OR `email` = ?';
+  const DUPLICATEquery = 'SELECT `username` FROM `users` WHERE `username` = ? OR `email` = ?';
   try {
     const results = await db.connect(DUPLICATEquery, [username, email]);
     const rows = await results[0];
-    if (rows.length > 0) {
-      // console.log("CheckForPreExisting: " + rows[0].username + " " + rows[0].email);
-      // console.log(Object.prototype.toString.call(rows));
-      return false;
-    } else {
-      //console.log("we're good to go!");  //return function;
+    if (rows[0] != undefined) {
+      //username or email exists
       return true;
+    } else {
+      //we're good to go
+      return false;
     }
   } catch (err) {
     throw err;
   }
 }
 
+/** Calls the addUser routine on the database to add the new user **/
 async function AddNewUser(params) {
   const ADDNEWUSERquery = 'CALL addUser(?, ?, ?, ?)';
   try {
@@ -37,31 +39,39 @@ async function AddNewUser(params) {
   }
 }
 
-router.post('/', async function (req, res) {
 
-  //check if the username or email already exist in the db
+router.post('/', async function (req, res) {
   try {
-    const results = await CheckForPreExistingUsers(req.body.username, req.body.email);
-    if (results == false) {
-      //TODO: Expand on this - need to properly indicate to client that 
-      //TODO: the username or email they were using already exists
-      //TODO: Use AJAX if possible.
-      //console.log("username or email was found");
-      res.redirect('back');
+    //check if the username or email already exist in the db
+    const isUserExist = await CheckForPreExistingUsers(req.body.username, req.body.email);
+    if (isUserExist == true) {
+      // TODO: Use WebSockets when possible to indicate the following
+      res.render('register.ejs', { message: "A user with that username or email id already exists"});
     } else {
-      //TODO: _Actually_ insert the new user in the db. Use addNewUser()
-      res.redirect('/');
+      // NOTE:  This test uses plaintext password comparision, which should be totally 
+      //        fine since we're not saving to disk
+      if (req.body.password != req.body.password2) {
+        // TODO: Use WebSockets when possible to indicate the following
+        res.render('register.ejs', { message: "Passwords do not match"});
+        return;
+      }
       const passwordHash = await psw.hashPassword(req.body.password);
-      //console.log(passwordHash);
 
       let user = new User(req.body.username, req.body.email);
       const params = [user.name, user.email, passwordHash, user.join_date];
       const results = await AddNewUser(params);
+      res.redirect('/');
     }
   } catch (err) {
-    throw err;
+    //TODO: Error Handling
+    res.render('error.ejs', {
+      status: 500,
+      message: err,
+      error: null,
+      stack: null
+    });
   }
-});
 
+});
 
 module.exports = router;
